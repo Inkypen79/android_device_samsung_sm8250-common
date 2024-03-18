5fd65b8f1405c3fc508a6f5e1126b34e1d2aaecc
Tim Zimmermann <tim@linux4.de>
2024-03-18 06:12:18 +0100

    sm8250-common: Add SPen AIDL to device framework matrix
    Change-Id: I1d44792437ad02924a849cb7672c6e0f67af8230

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,6 +138,7 @@ TARGET_PROVIDES_AUDIO_EXTNS := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(COMMON_PATH)/configs/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest.xml
