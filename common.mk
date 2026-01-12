bfaa9ca3ba0f6727a143ab0145015670d46ec34e
Nicholas Magill <nmagill1979@gmail.com>
2026-01-12 17:58:05 +0000

    sm8250-common: Revert of Gatekeeper MDFPP service changes
    * Sqaush of following commits
    
    Revert "sm8250-common: Build Gatekeeper MDFPP service"
    
    This reverts commit 16d9d48ab79ce233c25a6a1b630f0783bb7e9261.
    
    Revert "sm8250-common: Implement Gatekeeper MDFPP service"
    
    This reverts commit 8ae5e3d0740bf21a0708df257eabbefa91dfe3f8.
    
    Revert "sm8250-common: Ship QTI Gatekeeper HAL"
    
    This reverts commit a091d26ddd462e49785d1383d946f9735d598e83.

--- common.mk
+++ common.mk
@@ -186,7 +186,8 @@ endif
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-service-mdfpp
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
 
 # Health
 PRODUCT_PACKAGES += \
